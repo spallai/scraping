df10b0942b021f4c8fbc5137dabb08947474a3c9
SPallai <sangeeta.sumair@gmail.com>
2019-01-26 21:48:51 -0800

    scraped

---
+++ server.js
@@ -0,0 +1,60 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+//scraping tools
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+//require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+//initialize express
+var app = express();
+
+//configure middleware
+
+//use morgan logger for logging requests
+app.use(logger("dev"));
+
+//Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//connect to the Mongo DB
+mongoose.connect("mongodb://localhost/scraping", { useNewUrlParser: true });
+
+//ROUTES
+
+//A GET for scraping the USA Today website
+app.get("/scrape", function (req, res) {
+    //First we grab the body of the html with axios
+    axios.get("https://www.washingtonpost.com/").then(function (response) {
+        //then we load that into cheerio and save it with a $ as the selector
+        var $ = cheerio.load(response.data);
+
+        var results = [];
+
+        //we then grab ever title and link as such
+        $("div.headline").each(function (i, element) {
+            
+            var title = $(this).children().text();
+            var link = $(this).children("a").attr("href");
+            // var title = $(this).find("a").text();
+            // var link = $(this).find("a").attr("href");
+
+            results.push({
+                title: title,
+                link: link
+            });
+            console.log(results);
+        })
+    })
+    
+})
+
+app.listen(PORT, function(){
+    console.log("App is running on Port " + PORT);
+})
